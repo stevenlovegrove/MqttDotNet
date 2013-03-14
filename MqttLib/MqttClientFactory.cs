96f1fc5ec3079cc652fa2276dba837b6ebefa218
Kenneth Fuglsang <kfuglsang@gmail.com>
2013-03-14 22:52:56 +0100

    Updated overloads to client factory

--- MqttLib/MqttClientFactory.cs
+++ MqttLib/MqttClientFactory.cs
@@ -7,24 +7,14 @@ namespace MqttLib
 {
     public class MqttClientFactory
     {
-        public static IMqtt CreateClient(string connString, string clientId, string username, string password, IPersistence persistence)
+        public static IMqtt CreateClient(string connString, string clientId, string username = null, string password = null, IPersistence persistence = null)
         {
             return new Mqtt(connString, clientId, username, password, persistence);
         }
 
-        public static IMqtt CreateClient(string connString, string clientId)
-        {
-            return CreateClient(connString, clientId, null, null, null);
-        }
-
-        public static IMqttShared CreateSharedClient(string connString, string clientId, string username, string password)
+        public static IMqttShared CreateSharedClient(string connString, string clientId, string username = null, string password = null)
         {
             return new Mqtt(connString, clientId, username, password, null);
-        }
-
-        public static IMqttShared CreateSharedClient(string connString, string clientId)
-        {
-            return new Mqtt(connString, clientId, null, null, null);
         }
 
         public static IMqtt CreateBufferedClient(string connString, string clientId)
